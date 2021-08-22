e3a1e79592a4f509becfbeb6918028c644758bbc
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-23 01:30:35 +0200

    Feature updates
    Feature updates

--- src/sections/service-section.js
+++ src/sections/service-section.js
@@ -13,7 +13,7 @@ import {
 import { keyframes } from '@emotion/core';
 import TextFeature from 'components/text-feature';
 import ModalVideo from 'react-modal-video';
-import { IoIosPlay } from 'react-icons/io';
+import { IoIosPlay, IoMdHeartEmpty } from 'react-icons/io';
 
 import ServiceThumb from 'assets/service-thumb.png';
 import shapePattern from 'assets/shape-pattern1.png';
@@ -49,6 +49,7 @@ export default function ServiceSection() {
 		e.preventDefault();
 		setVideoOpen(true);
 	};
+	const [videoOpen, setVideoOpen] = useState(false);
 
 	return (
 		<section sx={{ variant: 'section.services' }}>
@@ -70,9 +71,29 @@ export default function ServiceSection() {
 				</Box>
 				<Box sx={styles.contentBox}>
 					<TextFeature subTitle={data.subTitle} title={data.title} />
-					<Grid></Grid>
+					<Grid sx={styles.grid}>
+						{data.features.map((feature) => (
+							<Box sx={styles.card} key={feature.id}>
+								<Image
+									src={feature.imgSrc}
+									alt={feature.altText}
+									sx={styles.icon}
+								/>
+								<Box sx={styles.wrapper}>
+									<Heading sx={styles.wrapper.title}>{feature.title}</Heading>
+									<Text sx={styles.wrapper.subTitle}>{feature.text}</Text>
+								</Box>
+							</Box>
+						))}
+					</Grid>
 				</Box>
 			</Container>
+			<ModalVideo
+				channel='youtube'
+				isOpen={videoOpen}
+				videoId='mxgJSNfENt4'
+				onClose={() => setVideoOpen(false)}
+			/>
 		</section>
 	);
 }
