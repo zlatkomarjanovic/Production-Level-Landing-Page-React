df55ca80b92ec1e973c177b613f316fba9d42a52
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-23 01:17:53 +0200

    Updates
    Updates

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -2,7 +2,27 @@ import React from 'react';
 import { Box, Text, Heading } from 'theme-ui';
 
 export default function SectionHeader({ title, slogan, isWhite }) {
-  return (
-    <h1>SectionHeader</h1>
-  );
+	return (
+		<Box sx={{ variant: 'sectionHeader' }}>
+			<Text
+				sx={{
+					variant: 'sectionHeader.subTitle',
+					color: isWhite ? 'white' : 'primary',
+					opacity: isWhite ? 0.7 : 1,
+				}}
+				as='p'
+			>
+				{slogan}
+			</Text>
+			<Heading
+				sx={{
+					variant: 'sectionHeader.title',
+					color: isWhite ? 'white' : 'heading',
+				}}
+				as='h2'
+			>
+				{title}
+			</Heading>
+		</Box>
+	);
 }
