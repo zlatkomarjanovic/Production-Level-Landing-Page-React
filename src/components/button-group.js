e8f0d491ec44c4e38567111076ad2b9fe7b01f01
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-23 18:26:53 +0200

    Major updates
    Major updates

--- src/components/button-group.js
+++ src/components/button-group.js
@@ -3,30 +3,41 @@ import { jsx, Box, Container, Flex } from 'theme-ui';
 import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io';
 
 export default function ButtonGroup({ next, previous }) {
-  return (
-    <h1>ButtonGroup</h1>
-  );
+	return (
+		<Flex sx={{ width: '100%' }}>
+			<Container>
+				<Box sx={styles.buttonGroup} className='button__group'>
+					<button aira-label='Previous' onClick={previous}>
+						<IoIosArrowRoundBack />
+					</button>
+					<button aira-label='Next' onClick={next}>
+						<IoIosArrowRoundForward />
+					</button>
+				</Box>
+			</Container>
+		</Flex>
+	);
 }
 
 const styles = {
-  buttonGroup: {
-    display: 'flex',
-    justifyContent: 'center',
-    mb: -4,
-    button: {
-      bg: 'transparent',
-      border: '0px solid',
-      fontSize: 40,
-      cursor: 'pointer',
-      px: '2px',
-      color: '#BBC7D7',
-      transition: 'all 0.25s',
-      '&:hover': {
-        color: 'text',
-      },
-      '&:focus': {
-        outline: 0,
-      },
-    },
-  },
+	buttonGroup: {
+		display: 'flex',
+		justifyContent: 'center',
+		mb: -4,
+		button: {
+			bg: 'transparent',
+			border: '0px solid',
+			fontSize: 40,
+			cursor: 'pointer',
+			px: '2px',
+			color: '#BBC7D7',
+			transition: 'all 0.25s',
+			'&:hover': {
+				color: 'text',
+			},
+			'&:focus': {
+				outline: 0,
+			},
+		},
+	},
 };
